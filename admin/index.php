743c7685fd5fee251db27ea7426d758687e7163d
selesdepselesnul <selesdepselesnul@gmail.com>
2016-06-04 21:24:44 +0700

    update

--- admin/index.php
+++ admin/index.php
@@ -1,10 +1,21 @@
 <?php
 require_once '../header.php';
+
+if($_POST['username']
+	&& $_POST['password']) {
+	mysqli_query(
+		$mysqli,
+		"UPDATE Admin 
+		 SET username='{$_POST['username']}'
+		 WHERE id=1;");
+}
+
 $result = mysqli_query($mysqli, 'SELECT * FROM Admin;');
 // $result = mysqli_use_result($mysqli);	
 $admin = mysqli_fetch_assoc($result);
+mysqli_close($mysqli); 
 ?>
-<div>
+<form method="POST">
 	<label for="username">Username</label>
 	<input type="text" name="username" 
 		   value="<?php echo $admin['username']?>" /> <br />
@@ -16,8 +27,9 @@ $admin = mysqli_fetch_assoc($result);
 			value="<?php echo $admin['email']?>" /> <br />
 	<label for="fullname">Fullname</label>
 	<input type="text" name="fullname" 
-			value="<?php echo $admin['fullname']?>"/>
-</div>
+			value="<?php echo $admin['fullname']?>"/> <br />
+	<input type="submit">
+</form>
 <?php
 require_once '../footer.php';
 ?>
