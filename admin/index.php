596f13d2af1131e31457d9cdeb269d9b4111d671
selesdepselesnul <selesdepselesnul@gmail.com>
2016-06-06 04:00:15 +0700

    update with post

--- admin/index.php
+++ admin/index.php
@@ -1,32 +1,37 @@
 <?php
 require_once '../header.php';
 
-if($_POST['username']
-	&& $_POST['password']) {
+if(!empty($_POST['username'])
+	&& !empty($_POST['password'])
+	&& !empty($_POST['email'])
+	&& !empty($_POST['fullname']))
 	mysqli_query(
 		$mysqli,
-		"UPDATE Admin 
-		 SET username='{$_POST['username']}'
-		 WHERE id=1;");
-}
+		"UPDATE Admin
+		SET
+		username='{$_POST['username']}',
+		password='{$_POST['password']}',
+		email='{$_POST['email']}',
+		fullname='{$_POST['fullname']}'
+		WHERE id=1;");
 
 $result = mysqli_query($mysqli, 'SELECT * FROM Admin;');
-// $result = mysqli_use_result($mysqli);	
+// $result = mysqli_use_result($mysqli);
 $admin = mysqli_fetch_assoc($result);
-mysqli_close($mysqli); 
+mysqli_close($mysqli);
 ?>
 <form method="POST">
 	<label for="username">Username</label>
-	<input type="text" name="username" 
+	<input type="text" name="username"
 		   value="<?php echo $admin['username']?>" /> <br />
 	<label for="password">Password</label>
-	<input type="password" name="password" 
+	<input type="password" name="password"
 			value="<?php echo $admin['password']?>" /> <br />
 	<label for="email">Email</label>
-	<input type="email" name="email" 
+	<input type="email" name="email"
 			value="<?php echo $admin['email']?>" /> <br />
 	<label for="fullname">Fullname</label>
-	<input type="text" name="fullname" 
+	<input type="text" name="fullname"
 			value="<?php echo $admin['fullname']?>"/> <br />
 	<input type="submit">
 </form>
--- config.php
+++ config.php
@@ -1,12 +1,12 @@
 <?php
-$config['base_url']='http://127.0.0.1:8080/uas_internet_dasar/';
+$config['base_url']='http://127.0.0.1:8080/uas/';
 $config['database']['hostname']='127.0.0.1';
 $config['database']['username']='root';
 $config['database']['password']='';
 $config['database']['database']='uas';
 
 $mysqli = mysqli_connect(
-			"127.0.0.1", 
-			"root", 
-			"indonesiaraya", 
+			"127.0.0.1",
+			"root",
+			"indonesiaraya",
 			"uas");
