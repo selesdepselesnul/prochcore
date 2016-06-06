9b54aa44f2c8af98e0407c60fae82e83344400db
selesdepselesnul <selesdepselesnul@gmail.com>
2016-06-06 10:54:12 +0700

    make it refresh after receiving posting request

--- admin/content.php
+++ admin/content.php
@@ -11,27 +11,53 @@ require_once '../tags.php';
     <hr />
     <h2>About</h2>
     <label for="about_header">Header</label>
-    <input type="text" name="about_header" value=""> <br />
+    <input type="text" name="about_header" value="<?php echo $content['about']['header']?>"> <br />
     <label for="about_content">Content</label>
-    <textarea name="about_content" rows="8" cols="40"></textarea><br />
+    <textarea name="about_content" rows="8" cols="40"><?php echo $content['about']['content']?></textarea><br />
     <hr />
     <h2>Contact</h2>
     <label for="header">header</label>
-    <input type="text" name="header" value=""> <br />
-    <label for="address_header">Address-Header</label>
-    <input type="text" name="address_header" value=""> <br />
-    <label for="address_content">Address-Content</label>
-    <textarea name="address_content" rows="8" cols="40"></textarea> <br />
-    <label for="social_media_header">Social Media-Header</label>
-    <input type="text" name="social_media_header" value=""> <br />
+    <input type="text" name="contact_header" value="<?php echo $content['contact']['header']?>"> <br />
+    <label for="contact_address_header">Address-Header</label>
+    <input type="text" name="contact_address_header" value="<?php echo $content['contact']['address_header']?>"> <br />
+    <label for="contact_address_content">Address-Content</label>
+    <textarea name="contact_address_content" rows="8" cols="40"><?php echo $content['contact']['address_content']?></textarea> <br />
+    <label for="contact_social_media_header">Social Media-Header</label>
+    <input type="text" name="contact_social_media_header" value="<?php echo $content['contact']['social_media_header']?>"> <br />
+    <label for="contact_form_header">Form-Header</label>
+    <input type="text" name="contact_form_header" value="<?php echo $content['contact']['form_header']?>"> <br />
     <input type="submit">
 </form>
 
 <?php
-    if(isset($_POST['home_header'])
-        && isset($_POST['home_content'])) {
-        update_content('Home',
-            ['header' => $_POST['home_header'],
-             'content' => $_POST['home_content']]);
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        if(isset($_POST['home_header'])
+            && isset($_POST['home_content'])) {
+            update_content('Home',
+                ['header' => $_POST['home_header'],
+                 'content' => $_POST['home_content']]);
+        }
+
+        if(isset($_POST['about_header'])
+            && isset($_POST['about_content'])) {
+            update_content('About',
+                ['header' => $_POST['about_header'],
+                 'content' => $_POST['about_content']]);
+        }
+
+        if(isset($_POST['contact_header'])
+            && isset($_POST['contact_address_header'])
+            && isset($_POST['contact_address_content'])
+            && isset($_POST['contact_social_media_header'])
+            && isset($_POST['contact_form_header'])) {
+            update_content('Contact',
+                ['header' => $_POST['contact_header'],
+                 'address_header' => $_POST['contact_address_header'],
+                 'address_content' => $_POST['contact_address_content'],
+                 'social_media_header' => $_POST['contact_social_media_header'],
+                 'form_header' => $_POST['contact_form_header']]);
+        }
+        header("refresh: 0;");
     }
+
 ?>
