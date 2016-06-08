9d0deebb9e1034951b781d13df85ea494a04cb01
selesdepselesnul <selesdepselesnul@gmail.com>
2016-06-08 21:05:32 +0700

    remove inboxs.php merge with inbox.php

--- inbox.php
+++ inbox.php
@@ -1,7 +1,36 @@
 <?php
 require_once 'header.php';
 require_once 'tags.php';
-if(isset($_GET['id']))
+
+define('LIMIT', 5);
+if (isset($_GET['page'])) {
+    $inboxs = read_all_assoc_limit('Inbox',
+        ($_GET['page'] * LIMIT) - LIMIT, LIMIT);
+?>
+    <table style="border:1;">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Waktu Masuk</th>
+                <th>Nama</th>
+                <th>Email</th>
+                <th>Content</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($inboxs as $inbox): ?>
+                <tr>
+                    <td><?php echo $inbox['id']?></td>
+                    <td><?php echo $inbox['message_time']?></td>
+                    <td><?php echo $inbox['name'] ?></td>
+                    <td><?php echo $inbox['email'] ?></td>
+                    <td><?php echo $inbox['content'] ?></td>
+                </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
+<?php
+} elseif(isset($_GET['id']))
     var_dump(read_dbase_by_id('Inbox', $_GET['id']));
 require_once 'footer.php';
 ?>
