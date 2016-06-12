29bcc1acbb8a24d3879628b0a355e2244f1fcc66
selesdepselesnul <selesdepselesnul@gmail.com>
2016-06-12 08:25:52 +0700

    change update_table to update_table_by_id in content.php

--- admin/content.php
+++ admin/content.php
@@ -135,7 +135,7 @@ $home_weapons = read_table('HomeWeapon');
 
     function updateIfNotEmpty($table_name, $post_name, $field_name) {
         if(!empty($_POST[$post_name]))
-            update_table($table_name, [
+            update_table_by_id($table_name, 1, [
                 $field_name => $_POST[$post_name]
             ]);
     }
