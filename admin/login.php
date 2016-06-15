5528f492590c7b1e85b3e17feecfe83d24701a65
selesdepselesnul <selesdepselesnul@gmail.com>
2016-06-16 05:27:57 +0700

    add alert message in login form if username and password doesn't match

--- admin/login.php
+++ admin/login.php
@@ -3,6 +3,7 @@ require_once '../function.php';
 require_once '../config.php';
 require_once 'header-admin.php';
 
+
 if(isset($_POST['username'])
     && isset($_POST['password'])) {
     $admin = read_row_by_id('Admin', 1);
@@ -11,6 +12,8 @@ if(isset($_POST['username'])
         session_start();
         $_SESSION['login'] = 1;
         header('Location: '.$config['base_url'].'admin/homeadmin.php');
+    } else {
+        $is_login_success = false;
     }
 }
 ?>
@@ -28,6 +31,14 @@ if(isset($_POST['username'])
                 <input type="submit" name="login" class="btn btn-default" value="login">
             </div>
         </form>
+        <?php if (isset($is_login_success)): ?>
+            <?php if (!$is_login_success):?>
+                <div class="alert alert-danger row">
+                    kombinasi username dan password salah
+                </div>
+            <?php endif ?>
+        <?php endif ?>
+
     </div>
 
 <?php require_once '../footer.php' ?>
